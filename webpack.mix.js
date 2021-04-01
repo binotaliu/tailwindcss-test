4cc053827dfafb08e4e1809e5691d44262f30518
BinotaLIU <binota@binota.org>
2021-04-01 20:29:19 +0800

    without nested

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix')
 
 mix.postCss('test.css', 'dist.css', [require('postcss-nested'), require('@tailwindcss/jit')])
+mix.postCss('test-without-nested.css', 'dist-without-nested.css', [require('@tailwindcss/jit')])
 
